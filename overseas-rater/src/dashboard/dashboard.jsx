ab6047a46a396352253bdf8f3b263c70d77caaf8
Ansh <137138957+Ansh-Kakadia@users.noreply.github.com>
2024-09-29 13:15:43 -0400

    Created Template Functions in dashboard.jsx

---
+++ overseas-rater/src/dashboard/dashboard.jsx
@@ -0,0 +1,18 @@
+
+
+
+function dashboardOption(id){
+    // scrape file for info using ID-number
+    // get image-url, program-name, program-location, program-term
+
+    const imageUrl = null
+    const programName = null
+    const programLocation = null
+    const programTerm = null
+}
+
+
+
+function dashboardElement(imageUrl, programName, programLocation, programTerm){
+    return null
+}
