55470ea0557298b16c2832ed137f2564b1ae54ad
Ansh <137138957+Ansh-Kakadia@users.noreply.github.com>
2024-10-08 19:08:42 -0400

    Basic Outline for dashboard.jsx

--- overseas-rater/src/App.jsx
+++ overseas-rater/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './dashboard/dashboard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
 
     </div>
   )
+  
 }
 
 export default App
--- overseas-rater/src/dashboard/dashboard.jsx
+++ overseas-rater/src/dashboard/dashboard.jsx
@@ -1,5 +1,9 @@
 
 
+// function testing(){
+//     const url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/1200px-Cute_dog.jpg"
+//     return dashboardElement(url, "Name", "Place", "Spring")
+// }
 
 function dashboardOption(id){
     // open file for info using ID-number
@@ -15,6 +19,11 @@ function dashboardOption(id){
 
 function dashboardElement(imageUrl, programName, programLocation, programTerm){
     return (
-        <></>
+        <div class="rounded-lg"> 
+            <h2>{programName}</h2>
+            <p>{programLocation}</p>
+            <p>{programTerm}</p>
+            <img src={imageUrl}  />
+        </div>
     )
 }
