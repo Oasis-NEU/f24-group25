bc13d41ba1f799e21e0867ad1ae8cd681c31218e
Ansh <137138957+Ansh-Kakadia@users.noreply.github.com>
2024-10-23 20:57:50 -0400

    Created Grid w/ .map + Misc
    dashboard.jsx --> dashboard-element.jsx
    moved placeholder for file reading from dashboard-element.jsx to dashboard-grid.jsx, allowing deletion of dashboardOption()

--- overseas-rater/src/App.jsx
+++ overseas-rater/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import DashboardOption from './dashboard/dashboard.jsx'
+import DashboardGrid from './dashboard/dashboard-grid.jsx'
 import Navbar from './navbar/navbar.jsx'
 
 function App() {
--- overseas-rater/src/dashboard/dashboard-grid.jsx
+++ overseas-rater/src/dashboard/dashboard-grid.jsx
@@ -1,5 +1,20 @@
-export default function DashboardGrid(ids){
-    <div class="grid-cols-5">
-        NEED TO ADD FUNCTIONALITY TO LOOP THROUGH IDS AND INSERT ELEMENTS
-    </div>
+import DashboardElement from "./dashboard-element"
+
+export default function DashboardGrid(ids) {
+    //turn ids into list of dictionaries, call opportunities, with this format:
+    // {
+    //     programName: String,
+    //     programLocation: String,
+    //     programTerm: String,
+    //     url: String
+    // }
+    let opportunities = []
+
+    return (
+        <div class="grid-cols-5">
+            {opportunities.map(opportunity => (<DashboardElement programName={opportunity.programName}
+                programLocation={opportunity.programLocation} programTerm={opportunity.programTerm}
+                imageUrl={opportunity.imageUrl} />))}
+        </div>
+    )
 }
