3954f48b1781e6fd9d2ce74bfe9fb8477e7db2eb
vinnykaushik <148011913+vinnykaushik@users.noreply.github.com>
2024-11-14 19:28:36 -0500

    Set a max height for the hero

--- overseas-rater/src/programInfo/hero.jsx
+++ overseas-rater/src/programInfo/hero.jsx
@@ -1,17 +1,33 @@
 function ProgramInfoHero({ imageUrl, programName }) {
   return (
-    <div
-      className="hero min-h-screen"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}>
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">{programName}</h1>
+      <div
+        style={{
+          maxHeight: '500px',
+          overflow: 'hidden',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '20px'
+        }}
+      >
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            maxHeight: '200px',
+            height: '100vh',
+          }}>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">{programName}</h1>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
