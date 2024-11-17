5a232f2995c6a8304b4e019bf579687d260e4d86
Ansh <137138957+Ansh-Kakadia@users.noreply.github.com>
2024-11-17 13:32:20 -0500

    outline for fuse.js search

--- overseas-rater/src/App.jsx
+++ overseas-rater/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <Navbar/>
       <Hero/>
-      <DashboardGrid searchTerm = "" /> 
-      <Program/>
+      <DashboardGrid  /> 
+
     </div>
   )
 
--- overseas-rater/src/dashboard/dashboard-grid.jsx
+++ overseas-rater/src/dashboard/dashboard-grid.jsx
@@ -1,25 +1,33 @@
-import DashboardElement from "./dashboard-element"
-import programList from '../program_list.json'
+import DashboardElement from "./dashboard-element";
+import programList from "../program_list.json";
+import Fuse from "fuse.js";
 
 export default function DashboardGrid(searchTerm = "") {
-    // Assuming programList is imported from your JSON file
-    const opportunities = programList.filter((opportunity) => (opportunity.title.includes(searchTerm)))
+  // Assuming programList is imported from your JSON file
+  // const opportunities = programList
 
-    
+  const fuse = new Fuse(programList, {
+    keys: ["title", "location"],
+    includeScore: true,
+    findAllMatches: true,
+    minMatchCharLength: 0,
+  });
 
-    return (
-        <div className="flex justify-center">
-            <div className="grid grid-cols-3 gap-20">
-                {opportunities.map(program => (
-                    <DashboardElement 
-                        key={program.id}
-                        title={program.title}
-                        location={program.location}
-                        semesters={program.semesters}
-                        image_base64={program.image_base64}
-                    />
-                ))}
-            </div>
-        </div>
-    )
+  const opportunities =  programList //fuse.search(searchTerm);
+
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 gap-20">
+        {opportunities.map((program) => (
+          <DashboardElement
+            key={program.id}
+            title={program.title}
+            location={program.location}
+            semesters={program.semesters}
+            image_base64={program.image_base64}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
